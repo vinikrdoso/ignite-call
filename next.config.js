c42b592a7422b46314a6b106b133ff04a8ff4c0a
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-06 18:37:26 +0100

    first commit: project setup

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+}
+
+export default nextConfig
---
+++ src/pages/index.page.tsx
@@ -0,0 +1,10 @@
+import { Button, Heading } from '@ignite-ui/react'
+
+export default function Home() {
+  return (
+    <>
+      <Heading as="h1">Hello World</Heading>
+      <Button>Click Me</Button>
+    </>
+  )
+}
