8a39c1feffcca49ec5e247d0a34ea3b06b8e062e
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-07 10:32:28 +0100

    feat: creation of home page with react hook form and zod for form handling and validation

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,1 @@
-import { Button, Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Heading as="h1">Hello World</Heading>
-      <Button>Click Me</Button>
-    </>
-  )
-}
+export { default } from './home'
