4b0fa2566ba4c916ef9faa50433476e070574334
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-13 18:17:10 +0100

    feat: add next-auth and handle authentication

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -50,6 +50,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (err) {
       console.log(err)
 
