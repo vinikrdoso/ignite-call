76a9a97df6e3362720552924ae645166b32a874d
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-14 20:06:38 +0100

    feat: month navigation inside calendar using dayjs

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,6 +1,6 @@
-import { ConfirmStep } from './ConfirmStep'
-// import { CalendarStep } from './CalendarStep'
+// import { ConfirmStep } from './ConfirmStep'
+import { CalendarStep } from './CalendarStep'
 
 export function ScheduleForm() {
-  return <ConfirmStep />
+  return <CalendarStep />
 }
